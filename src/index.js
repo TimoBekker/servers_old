7dd2e1ab01ce73ac1be8c45b227657861793e273
TimoBekker <nikiforov93@mail.ru>
2025-07-29 10:15:59 +0400

    Fix Ant Design locale import path

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
-import ruRU from 'antd/locale/ru_RU';
-import 'antd/dist/antd.css';
+import ruRU from 'antd/lib/locale/ru_RU';
+import 'antd/dist/reset.css';
 import './index.css';
 import App from './App';
 
